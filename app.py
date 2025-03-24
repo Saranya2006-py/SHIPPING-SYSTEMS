0f46df4a228de0c0f0e4a9782597bae7badf014e
Saranya2006-py <saranyatd2006@gmail.com>
2025-03-24 20:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import os
+import sys
+
+# Absolute path to your project root
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, PROJECT_ROOT)
+
 from tracking_system.wsgi import application
 
-# Gunicorn expects a variable named `app`
 app = application
